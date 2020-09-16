794882322589a69fe9893d0b122fd19d0cdf2c1a
Jenni <jenniePaskua@gmail.com>
2020-09-16 21:35:57 +0900

    ✨init toy project

---
+++ toyproject/toyproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toyproject
+//
+//  Created by jeeeun.jo on 2020/09/16.
+//  Copyright © 2020 jenniePaskua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
